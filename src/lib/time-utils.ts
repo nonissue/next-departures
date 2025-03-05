864b6be0a1a815cdeac9b370923287317db97377
nonissue <andy@nonissue.org>
2025-03-05 02:02:31 -0700

    done for now

--- src/lib/time-utils.ts
+++ src/lib/time-utils.ts
@@ -1,9 +1,22 @@
+import { format } from 'path';
+import { padLeadingZeros } from '../utils.js';
+
 export const getCurrentDate = () => {
   const currentDate = new Date();
   const formattedDate =
     currentDate.getFullYear() * 10000 +
     (currentDate.getMonth() + 1) * 100 +
     currentDate.getDate();
+  return formattedDate;
+};
+
+export const getYesterdaysDate = () => {
+  const currentDate = new Date();
+  const formattedDate =
+    currentDate.getFullYear() * 10000 +
+    (currentDate.getMonth() + 1) * 100 +
+    currentDate.getDate() -
+    1;
   return formattedDate;
 };
 
@@ -14,4 +27,56 @@ export const getCurrentTime = () => {
   });
 
   return currentTimeFormatted;
+};
+
+/**
+ * Validates the configuration object for GTFS import
+ * @param interval the interval between startTime and endTime (minutes, default = 60)
+ * @throws Error if agencies are missing or if agency lacks both url and path
+ * @returns an object containing formatted start and stop times
+ */
+export const getStartAndStopTimeFormatted = (interval: number = 60) => {
+  const targetTime = '23:30:00'; // this could be startTime.toLocaleString etc to get current
+  // const targetTime = new Date().toLocaleTimeString('eo', { hour12: false });
+  const [hours, minutes, seconds] = targetTime.split(':').map(Number);
+  const SERVICE_DAY_END_HOUR = 5;
+  const normalizedStart = `${hours < SERVICE_DAY_END_HOUR ? hours + 24 : hours}:${minutes}:${seconds}`;
+  const normalizedEnd = `${(hours < SERVICE_DAY_END_HOUR ? hours + 24 : hours) + 1}:${minutes}:${seconds}`;
+  // const tmp2 = `${hours < SERVICE_DAY_END_HOUR ? hours + 24 : hours}:${minutes}:${seconds}`;
+
+  // const startTime = new Date('23:00:00');
+  const startTime = new Date();
+  /*
+  GTFS spec says:
+  
+  if startTime hours is less than, say, 4, add 24 to it
+  eg. 00:30:00 becomes 24:30:00 
+  
+
+*/
+
+  const tmp = `${startTime.getHours() + 1}:${startTime.getMinutes()}:${startTime.getSeconds()}`;
+  // console.log(padLeadingZeros(tmp2));
+
+  const formattedTimes = {
+    // this needs to be modified ->
+    // if our startTime + interval occurs after midnight
+    //    eg. startTime: 23:45:00 | +60m | endTime: 00:45:00
+    // But occurs on the same *service day*, the endTime
+    //    eg. startTime: 23:45:00 | +60m | endTime: 24:45:00
+    // end: new Date(
+    //   startTime.getTime() + interval * 60 * 1000,
+    // enter the end time as a value greater than 24:00:00
+    // ).toLocaleTimeString('eo', { hour12: false }),
+    // start: startTime.toLocaleTimeString('eo', {
+    //   hour12: false,
+    // }),
+    // end: padLeadingZeros(tmp),
+    // start: targetTime,
+    // end: padLeadingZeros(tmp2),
+    start: normalizedStart,
+    end: normalizedEnd,
+  };
+
+  return formattedTimes;
 };
--- src/scratch.ts
+++ src/scratch.ts
@@ -3,7 +3,11 @@ import { getConfig } from './utils.js';
 import test from 'node:test';
 import { importGtfsDataToDb, loadDb } from './db.js';
 import { Config } from './types/global.js';
-import { getCurrentDate, getCurrentTime } from './lib/time-utils.js';
+import {
+  getCurrentDate,
+  getStartAndStopTimeFormatted,
+  getYesterdaysDate,
+} from './lib/time-utils.js';
 
 const previewConfig = async () => {
   const config = await getConfig();
@@ -45,12 +49,9 @@ const runner = async () => {
   };
 
   const openRunClose = async (funcToRun: Function) => {
-    console.log('openRunClose');
-
     const config: Config = await getConfig();
 
     const db = await loadDb(config);
-    console.log('Db loaded');
 
     funcToRun();
 
@@ -58,34 +59,77 @@ const runner = async () => {
   };
 
   const testFunc = () => {
-    const stopTimesNorth = getStoptimes(
+    const tripLookaheadIntervalMins = 60;
+    const currentDate = getCurrentDate();
+    const yesterdaysDate = getYesterdaysDate();
+    const formattedTimes = getStartAndStopTimeFormatted(
+      tripLookaheadIntervalMins,
+    );
+
+    console.log(formattedTimes);
+    let coronaSouth;
+
+    coronaSouth = getStoptimes(
       {
         stop_id: '1891', // Corona Southbound
-        date: getCurrentDate(),
-        start_time: '21:42:00',
-        end_time: '22:42:00',
+        date: currentDate,
+        start_time: formattedTimes.start,
+        end_time: formattedTimes.end,
+        // end_time: '25:59:59',
       },
       ['trip_id', 'stop_headsign', 'departure_time'],
       [['departure_time', 'ASC']],
     );
+    console.log(coronaSouth.length);
 
-    const stoptimesSouth = getStoptimes(
+    if (coronaSouth.length == 0) {
+      console.log('TRYING AGAIN');
+      coronaSouth = getStoptimes(
+        {
+          stop_id: '1891', // Corona Southbound
+          date: 20250304,
+          start_time: formattedTimes.start,
+          end_time: formattedTimes.end,
+          // end_time: '25:59:59',
+        },
+        ['trip_id', 'stop_headsign', 'departure_time'],
+        [['departure_time', 'ASC']],
+      );
+    }
+
+    console.log('\nCorona Station / SOUTH');
+
+    coronaSouth.forEach((entry) => {
+      const { stop_headsign, departure_time } = entry;
+      console.log(
+        `${stop_headsign?.padEnd(20)} - ${departure_time?.padStart(10)}`,
+      );
+    });
+
+    // console.log(coronaSouth);
+
+    const southgateNorth = getStoptimes(
       {
         stop_id: '2114', // Southgate Northbound
-        date: getCurrentDate(),
-        start_time: '21:42:00',
-        end_time: '22:42:00',
+        date: currentDate,
+        start_time: formattedTimes.start,
+        end_time: formattedTimes.end,
       },
       ['trip_id', 'stop_headsign', 'departure_time'],
       [['departure_time', 'ASC']],
     );
-
-    console.log(stoptimesSouth);
+    console.log('\nSouthgate STN / NORTH');
+    southgateNorth.forEach((entry) => {
+      const { stop_headsign, departure_time } = entry;
+      console.log(
+        `${stop_headsign?.padEnd(20)} - ${departure_time?.padStart(10)}`,
+      );
+    });
   };
 
   //   await runLoadDb();
-  //   await openRunClose(testFunc);
-  console.log(getCurrentTime());
+  await openRunClose(testFunc);
+  // console.log(getCurrentTime());
 };
 
 runner();
