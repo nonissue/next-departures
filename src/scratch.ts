f5c41b0f9879c00d0b2789e50eeb5df71fdc7a6a
nonissue <andy@nonissue.org>
2025-03-07 00:08:32 -0700

    bump

--- src/scratch.ts
+++ src/scratch.ts
@@ -1,4 +1,11 @@
-import { closeDb, getAgencies, getStops, getStoptimes, openDb } from 'gtfs';
+import {
+  closeDb,
+  getAgencies,
+  getStops,
+  getStoptimes,
+  openDb,
+  advancedQuery,
+} from 'gtfs';
 import { getConfig } from './utils.js';
 import { loadDb } from './db.js';
 import { Config } from './types/global.js';
@@ -9,6 +16,7 @@ import {
   getStartAndStopTimeFormatted,
 } from './lib/time-utils.js';
 import { getDeparturesForStop } from './getDeparturesForStop.js';
+import { getStopName } from './getStopName.js';
 
 const previewConfig = async () => {
   const config = await getConfig();
@@ -38,15 +46,6 @@ export const openRunClose = async (funcToRun: Function) => {
   funcToRun();
 
   closeDb(db);
-};
-
-const runner = async () => {
-  const config: Config = await getConfig();
-
-  const db = await loadDb(config);
-  await getDeparturesForStop('1926');
-  closeDb(db);
-  // await openRunClose(testFunc);
 };
 
 const testFunc = async () => {
@@ -103,4 +102,62 @@ const testFunc = async () => {
   });
 };
 
+const advancedQueryTest = async () => {
+  // Example `advancedQuery` joining stop_times with trips.
+  const advancedQueryOptions = {
+    query: {
+      'stop_times.trip_id': '28359464',
+    },
+    fields: ['stop_times.stop_headsign', 'stop_times.trip_id', 'arrival_time'],
+    join: [
+      {
+        type: 'INNER',
+        table: 'trips',
+        on: 'stop_times.trip_id=trips.trip_id',
+      },
+    ],
+  };
+
+  const stopTimes = advancedQuery('stop_times', advancedQueryOptions);
+
+  console.log(stopTimes);
+  // stopTimes.forEach((entry) => {
+  //   const { stop_headsign, departure_time } = entry;
+  //   console.log(
+  //     `${stop_headsign?.padEnd(20)} - ${departure_time?.padStart(10)}`,
+  //   );
+  // });
+};
+
+/* returns
+  {
+    stop_headsign: 'Clareview',
+    stop_name: 'Southgate Station',
+    arrival_time: '17:57:00'
+  },
+  */
+const advancedQueryStoptimesStops = async () => {
+  const advancedQueryOptions = {
+    query: {
+      'stop_times.stop_id': '2114',
+    },
+    fields: ['stop_times.stop_headsign', 'stops.stop_name', 'arrival_time'],
+    join: [
+      { type: 'INNER', table: 'stops', on: 'stop_times.stop_id=stops.stop_id' },
+    ],
+  };
+  const stopTimes = advancedQuery('stop_times', advancedQueryOptions);
+  console.log(stopTimes);
+};
+
+const runner = async () => {
+  const config: Config = await getConfig();
+
+  const db = await loadDb(config);
+  // await getDeparturesForStop('1926');
+  // await getStopName('1926');
+  await advancedQueryStoptimesStops();
+  closeDb(db);
+  // await openRunClose(testFunc);
+};
 runner();
