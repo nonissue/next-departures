9a8973355c63b20ff10ec12eea33645795040e67
nonissue <andy@nonissue.org>
2025-03-07 00:05:51 -0700

    add some new utilties

---
+++ src/getStopName.ts
@@ -0,0 +1,30 @@
+import { closeDb, getStopAttributes, getStops } from 'gtfs';
+import { loadDb } from './db.js';
+
+export const getStopName = async (stopId: string) => {
+  let stopFields;
+
+  stopFields = getStops(
+    {
+      stop_id: stopId, // Corona Southbound
+    },
+    // ['stop_id', 'stop_name'],
+    [],
+    [],
+  );
+  console.log(stopFields);
+
+  // let test = await getStopAttributes({ stop_id: '2114' });
+  // test = await getStopAttributes();
+  // console.log(test);
+
+  stopFields.forEach((entry) => {
+    const { stop_id, stop_name } = entry;
+    console.log(`${stop_id?.padEnd(10)} ${stop_name?.padEnd(30)}`);
+  });
+
+  // test.forEach((entry) => {
+  //   const { stop_id, stop_name } = entry;
+  //   console.log(`${stop_id?.padEnd(10)} ${stop_name?.padEnd(30)}`);
+  // });
+};
