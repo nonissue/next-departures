b5a9e19e4f431f1d7c8e4fa1026dcfa4ae276f86
nonissue <andy@nonissue.org>
2025-03-07 00:05:39 -0700

    cleanup

--- src/getDeparturesForStop.ts
+++ src/getDeparturesForStop.ts
@@ -25,60 +25,6 @@ const openRunClose = async (funcToRun: Function) => {
  * @throws Error if stopId isn't provided
  * @returns Nothing
  */
-export const TRASHgetDeparturesForStop = async (stopId: string) => {
-  if (!stopId) {
-    throw new Error('stopId is required!');
-  }
-
-  const tripLookaheadIntervalMins = 60;
-  const currentServiceDate = getCurrentServiceDate();
-
-  const formattedTimes = getStartAndStopTimeFormatted(
-    tripLookaheadIntervalMins,
-  );
-
-  let departuresForStop;
-
-  departuresForStop = getStoptimes(
-    {
-      stop_id: '1891', // Corona Southbound
-      date: currentServiceDate,
-      start_time: formattedTimes.start,
-      end_time: formattedTimes.end,
-      // end_time: '25:59:59',
-    },
-    ['trip_id', 'stop_headsign', 'departure_time'],
-    [['departure_time', 'ASC']],
-  );
-  console.log(departuresForStop.length);
-
-  if (departuresForStop.length == 0) {
-    console.log('TRYING AGAIN');
-    departuresForStop = getStoptimes(
-      {
-        stop_id: '1891', // Corona Southbound
-        date: 20250304,
-        start_time: formattedTimes.start,
-        end_time: formattedTimes.end,
-        // end_time: '25:59:59',
-      },
-      ['trip_id', 'stop_headsign', 'departure_time'],
-      [['departure_time', 'ASC']],
-    );
-  }
-
-  console.log('\nCorona Station / SOUTH');
-
-  departuresForStop.forEach((entry) => {
-    const { stop_headsign, departure_time } = entry;
-    console.log(
-      `${stop_headsign?.padEnd(20)} - ${departure_time?.padStart(10)}`,
-    );
-  });
-
-  // await openRunClose(getDeparturesForStop);
-};
-
 export const getDeparturesForStop = (
   stopId: string,
   tripLookaheadIntervalMins: number = 60,
