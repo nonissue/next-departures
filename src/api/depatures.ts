ce2f71c1460ed51bd03edda4d682558d08b672e7
nonissue <andy@nonissue.org>
2025-07-26 00:55:26 -0600

    clean up departures, remove unused database access methods

--- src/api/depatures.ts
+++ src/api/depatures.ts
@@ -1,9 +1,6 @@
 // src/api/departures.ts
 import express from 'express';
-import { Config, openDb } from 'gtfs';
-import { importGtfsDataToDb, loadDb } from '@/lib/db-utils';
 import { getNearbyDepartures } from '@/lib/get-nearby-departures';
-import { getConfig } from '@/lib/utils';
 
 const router = express.Router();
 
@@ -16,32 +13,8 @@ router.get('/api/departures', async (req, res) => {
     }
 
     try {
-        const currentTime = new Date();
-        const config: Config = await getConfig();
-        const db = await loadDb(config);
-
         const nearbyDeparturesResult = await getNearbyDepartures({ lat, lon });
-
-        // const station = await getNearestStation({ lat, lon });
-        // const stops = await getStopsForParentStation(station.stop_id);
-
-        // const allDepartures = [];
-
-        // for (const stop of stops) {
-        //     const departures = await getDeparturesForStop(stop.stop_id);
-        //     for (const dep of departures) {
-        //         allDepartures.push({
-        //             time: dep.departure_time,
-        //             destination: dep.stop_headsign,
-        //         });
-        //     }
-        // }
-
-        // allDepartures.sort((a, b) => a.time.localeCompare(b.time));
-
         res.json(nearbyDeparturesResult);
-
-        // res.json());
     } catch (error) {
         console.error('Error in /api/departures:', error);
         res.status(500).json({ error: 'Failed to fetch departures' });
