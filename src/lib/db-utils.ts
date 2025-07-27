b20ff574b71184caadc1d399091e8bc7385e5c04
nonissue <andy@nonissue.org>
2025-07-26 21:18:15 -0600

    formatting

--- src/lib/db-utils.ts
+++ src/lib/db-utils.ts
@@ -3,56 +3,56 @@ import { validateConfigForImport } from '@/lib/file-utils';
 import { Config } from '@/types/global';
 
 export const loadDb = async (config: Config) => {
-  // const config = await fetchConfig();
-  validateConfigForImport(config);
+    // const config = await fetchConfig();
+    validateConfigForImport(config);
 
-  if (!config.sqlitePath) {
-    throw new Error(
-      `To load and connect to an existing database, config.json must contain a valid \`sqlitePath\``,
-    );
-  }
+    if (!config.sqlitePath) {
+        throw new Error(
+            `To load and connect to an existing database, config.json must contain a valid \`sqlitePath\``
+        );
+    }
 
-  if (!config.agencies.length) {
-    throw new Error('No agency defined in `config.json`');
-  }
+    if (!config.agencies.length) {
+        throw new Error('No agency defined in `config.json`');
+    }
 
-  let db;
+    let db;
 
-  try {
-    db = openDb(config);
-  } catch (error) {
-    console.error(
-      `Unable to open sqlite database "${config.sqlitePath}" defined as \`sqlitePath\` config.json. Ensure the parent directory exists and import GTFS before running this app.`,
-    );
-    throw new Error('Error opening database');
-  }
+    try {
+        db = openDb(config);
+    } catch (error) {
+        console.error(
+            `Unable to open sqlite database "${config.sqlitePath}" defined as \`sqlitePath\` config.json. Ensure the parent directory exists and import GTFS before running this app.`
+        );
+        throw new Error('Error opening database');
+    }
 
-  return db;
+    return db;
 };
 
 export const importGtfsDataToDb = async (config: Config) => {
-  console.log('config from importGtfsToDb');
-  console.log(config);
+    console.log('config from importGtfsToDb');
+    console.log(config);
 
-  console.time('GTFS Import Duration'); // Start the timer
+    console.time('GTFS Import Duration'); // Start the timer
 
-  let db;
+    let db;
 
-  try {
-    db = await loadDb(config);
-  } catch (error: any) {
-    throw new Error(error);
-  }
+    try {
+        db = await loadDb(config);
+    } catch (error: any) {
+        throw new Error(error);
+    }
 
-  // console.log(
-  //   `Starting GTFS import for ${config.agencies.length} agency(s) using SQLite database at ${config.sqlitePath}`,
-  // );
+    // console.log(
+    //   `Starting GTFS import for ${config.agencies.length} agency(s) using SQLite database at ${config.sqlitePath}`,
+    // );
 
-  try {
-    await importGtfs({ agencies: config.agencies, db: db });
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        await importGtfs({ agencies: config.agencies, db: db });
+    } catch (error) {
+        console.error(error);
+    }
 
-  console.timeEnd('GTFS Import Duration'); // End the timer and log duration
+    console.timeEnd('GTFS Import Duration'); // End the timer and log duration
 };
--- src/lib/stop-utils.ts
+++ src/lib/stop-utils.ts
@@ -12,17 +12,17 @@ import { getServiceDate, getServiceTime } from '@/lib/time-utils';
  * @returns Promise<Stop[]> - an array of stop objects representing transit stations.
  */
 export const getTransitStations = (db: Database): Stop[] => {
-  const stations = `
+    const stations = `
     SELECT *
     FROM stops
     WHERE location_type = 1
     ORDER BY stops.stop_name;
   `;
 
-  let queryToRun = stations;
-  const queryResults = db.prepare(queryToRun).all();
+    let queryToRun = stations;
+    const queryResults = db.prepare(queryToRun).all();
 
-  return queryResults as Stop[];
+    return queryResults as Stop[];
 };
 
 /**
@@ -32,27 +32,65 @@ export const getTransitStations = (db: Database): Stop[] => {
  * @returns Promise<Stop[]> - an array of stop objects representing transit stations.
  */
 export const getClosestStation = ({ lat, lon }: GeoCoordinate = {}): Stop => {
-  const nearbyStations = getStops(
-    {
-      location_type: 1,
-      stop_lat: lat,
-      stop_lon: lon,
-    },
-    [],
-    [],
-    {
-      bounding_box_side_m: STATION_SEARCH_BOUNDING_BOX_AREA,
-    },
-  );
+    const nearbyStations = getStops(
+        {
+            location_type: 1,
+            stop_lat: lat,
+            stop_lon: lon,
+        },
+        [],
+        [],
+        {
+            bounding_box_side_m: STATION_SEARCH_BOUNDING_BOX_AREA,
+        }
+    );
 
-  return nearbyStations[0];
+    return nearbyStations[0];
 };
 
 export const getStopsForParentStation = (parent_station_id: string): Stop[] => {
-  const platforms = getStops({
-    parent_station: parent_station_id,
-    location_type: 0,
-  });
+    const platforms = getStops({
+        parent_station: parent_station_id,
+        location_type: 0,
+    });
 
-  return [...platforms];
+    return [...platforms];
+};
+
+/**
+ * getDeparturesForStop
+ * @param stopId - string - `node-gtfs` docs type this as string
+ * @param targetTime - string (optional) - timestamp to run query at specific time
+ * @param tripLookaheadIntervalMins - number - not implemented yet
+ * @throws Error if stopId isn't provided
+ * @returns Array of Stoptimes[] containn objects for each matching upcoming departures for specified stop at current time (or at targetTime, if provided)
+ */
+export const getDeparturesForStop = async (
+    stopId: string,
+    targetTime?: string,
+    tripLookaheadIntervalMins: number = 60
+): Promise<StopDepartures[]> => {
+    if (!stopId) throw new Error('stopId is required');
+
+    const [currentServiceDate, currentServiceTime] = targetTime
+        ? [getServiceDate({ targetTime }), getServiceTime(targetTime)]
+        : [getServiceDate(), getServiceTime()];
+
+    const departures = getStoptimes(
+        {
+            stop_id: stopId,
+            date: currentServiceDate,
+            start_time: currentServiceTime,
+        },
+        [
+            'stop_id',
+            'trip_id',
+            'stop_headsign',
+            'departure_time',
+            'departure_timestamp',
+        ],
+        [['departure_time', 'ASC']]
+    ) as StopDepartures[];
+
+    return departures;
 };
