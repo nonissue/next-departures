4f35329f1ced3cea5587f6bbf69d17217d244977
nonissue <andy@nonissue.org>
2025-07-26 22:33:43 -0600

    move utils.test.ts -> file-utils.test.ts since we renamed utils.ts to file-utils.ts

---
+++ src/lib/file-utils.test.ts
@@ -0,0 +1,14 @@
+import { it, describe, beforeEach, afterEach, expect, vi } from 'vitest';
+import { validateConfigForImport } from '@/lib/file-utils';
+
+describe('time-utils', () => {
+    beforeEach(() => {});
+
+    afterEach(() => {});
+
+    it('validateConfigForImport should return the current date in the correct format', () => {
+        // Mock fake system date to specific date for testing
+        const fakeConfig = {};
+        expect('something').toBe('something');
+    });
+});
