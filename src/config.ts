32360715132454c819d24da42cd8c1cc49125f23
nonissue <andy@nonissue.org>
2025-08-02 00:28:24 -0600

    some constants werent so constant. moved non-constant constants to `config.ts` for now

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,8 @@
-export const SERVICE_DAY_END_HOUR = 5;
+export const SERVICE_DAY_START_HOUR = 5;
+export const SERVICE_DAY_END_HOUR = 4;
 export const STATION_SEARCH_BOUNDING_BOX_AREA = 5000;
+export const TEST_COORDS = { lat: 53.542005, lon: -113.506607 };
+export const TEST_COORDS_SG = { lat: 53.485704, lon: -113.51379 };
+export const DEFAULT_LOOK_AHEAD_IN_MINS = 120;
+export const DEFAULT_STOP_COUNT_LIMIT = 5;
+export const DEFAULT_TIMEZONE = 'America/Edmonton';
