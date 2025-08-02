66dafc0226564168d3fbf1261ffdf2738963a430
nonissue <andy@nonissue.org>
2025-08-02 00:37:07 -0600

    Tweak default stop count limit

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,5 @@ export const STATION_SEARCH_BOUNDING_BOX_AREA = 5000;
 export const TEST_COORDS = { lat: 53.542005, lon: -113.506607 };
 export const TEST_COORDS_SG = { lat: 53.485704, lon: -113.51379 };
 export const DEFAULT_LOOK_AHEAD_IN_MINS = 120;
-export const DEFAULT_STOP_COUNT_LIMIT = 5;
+export const DEFAULT_STOP_COUNT_LIMIT = 7;
 export const DEFAULT_TIMEZONE = 'America/Edmonton';
